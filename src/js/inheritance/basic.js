3806baecc401bd86523e9daa5c153e1ef4a5932e
Eugene <job.valchkou@gmail.com>
2014-02-04 01:02:51 -0600

    inheritance

---
+++ src/js/inheritance/basic.js
@@ -0,0 +1,35 @@
+/**
+ * Very basic way to implement inheritance in JS.
+ * Assign parent instance to the Child.prototype
+ * Features:
+ * 		Child will inherit both prototype and own properties from the Parent instance.
+ * 		If Parent allows parameters in constructor the Child will not pass them.
+ * 
+ */
+(function() {
+	
+	/****** Define Parent *****/
+	function Parent(name) {
+		this.name = name || 'I am Parent';
+	}
+	
+	/*** add function to the Parent prototype ***/
+	Parent.prototype.getName = function () {
+		return this.name;
+	};
+	
+	/****** Define Child *****/
+	function Child(name) {};
+	
+	function extend(ChildClass, ParentClass) {
+		ChildClass.prototype = new ParentClass();
+	}
+	
+	// result
+	var child = new Child();
+	child.getName(); // 'I am Parent' . getName was inherited
+	
+	var child = new Child("I am Child");
+	child.getName(); // 'I am Parent'. Child name get lost
+	
+})();
